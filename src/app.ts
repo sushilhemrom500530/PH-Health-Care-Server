642826358be1f3fcecb732b8acde7d8c9e072e76
Sushil Hemrom <sushilhemrom98@gmail.com>
2025-04-08 09:42:51 +0600

    add project initiate and install package and runinning server

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express"
+import cors from "cors"
+
+const app: Application = express();
+
+app.use(cors())
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        message: "Ph Health Care Server...."
+    })
+})
+
+export default app;
