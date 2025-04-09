a9264b338f39555027a16393f6c1fb148139d520
Sushil Hemrom <sushilhemrom98@gmail.com>
2025-04-09 11:35:48 +0600

    add customize code in admin route

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,25 +1,47 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const getAllFromDB = async (params: any) => {
-    const result = await prisma.admin.findMany({
-        where: {
-            OR: [
-                {
-                    name: {
-                        contains: params.searchTerm,
-                        mode: "insensitive"
-                    }
-                },
-                {
-                    email: {
-                        contains: params.searchTerm,
-                        mode: "insensitive"
-                    }
+
+    const andConditions: Prisma.AdminWhereInput[] = [];
+
+
+    // [
+    //     {
+    //         name: {
+    //             contains: params.searchTerm,
+    //             mode: "insensitive"
+    //         }
+    //     },
+    //     {
+    //         email: {
+    //             contains: params.searchTerm,
+    //             mode: "insensitive"
+    //         }
+    //     }
+    // ]
+
+    const adminSearchAbleField = ['name', 'email']
+
+    if (params.searchTerm) {
+        andConditions.push({
+            OR: adminSearchAbleField.map(field => ({
+                [field]: {
+                    contains: params.searchTerm,
+                    mode: "insensitive"
                 }
-            ]
-        }
+            }))
+        })
+    }
+
+
+    console.dir(andConditions, { depth: "inifinity" })
+
+    const whereConditions: Prisma.AdminWhereInput = { AND: andConditions }
+
+    const result = await prisma.admin.findMany({
+        where: whereConditions
     });
     return result;
 }
