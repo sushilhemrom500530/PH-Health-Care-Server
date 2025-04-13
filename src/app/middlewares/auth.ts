d836edb700a996c4974a9b075efe1be1812afe60
Sushil Hemrom <sushilhemrom98@gmail.com>
2025-04-13 20:24:14 +0600

    add api error

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import { jwtHelpers } from "../../helpers/jwtHelpers";
+import config from "../../config";
+import { Secret } from "jsonwebtoken";
+import apiError from "../error/apiError";
+import status from "http-status";
+
+const auth = (...roles: string[]) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const token = req.headers.authorization;
+            // console.log({ token })
+            if (!token) {
+                throw new apiError(status.UNAUTHORIZED, "You are not authorized!")
+            }
+
+            const verifiedUser = jwtHelpers.verifyToken(token, config.jwt.refresh_token_secret as Secret)
+            // jwtHelpers.verifyToken(token, config.jwt.jwt_secret as Secret)
+
+            // req.user = verifiedUser;
+            // console.log({ verifiedUser })
+
+            if (roles.length && !roles.includes(verifiedUser?.role)) {
+                throw new apiError(status.FORBIDDEN, "Forbidden access!")
+            }
+            next()
+        }
+        catch (err) {
+            next(err)
+        }
+    }
+};
+
+export default auth;
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,38 +1,12 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import { userController } from "./user.controller";
-import { jwtHelpers } from "../../../helpers/jwtHelpers";
-import config from "../../../config";
-import { Secret } from "jsonwebtoken";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
-const auth = (...roles: string[]) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const token = req.headers.authorization;
-            // console.log({ token })
-            if (!token) {
-                throw new Error("You are not authorized!")
-            }
 
-            const verifiedUser = jwtHelpers.verifyToken(token, config.jwt.refresh_token_secret as Secret)
-            // jwtHelpers.verifyToken(token, config.jwt.jwt_secret as Secret)
 
-            // req.user = verifiedUser;
-            // console.log({ verifiedUser })
-
-            if (roles.length && !roles.includes(verifiedUser?.role)) {
-                throw new Error("Forbidden!")
-            }
-            next()
-        }
-        catch (err) {
-            next(err)
-        }
-    }
-}
-
-router.post("/", auth('ADMIN', 'DOCTOR'), userController.createAdmin)
+router.post("/", auth('ADMIN', 'SUPER_ADMIN'), userController.createAdmin)
 
 
 export const userRoutes = router;
