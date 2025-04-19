3d75903d65e99ee6e4ffe2f7946f7a813208a347
Sushil Hemrom <sushilhemrom98@gmail.com>
2025-04-19 08:34:08 +0600

    add create doctor constant and set get route also filter it

---
+++ src/app/modules/doctor/doctor.constant.ts
@@ -0,0 +1,19 @@
+export const doctorSearchableFields: string[] = [
+    'name',
+    'email',
+    'contactNumber',
+    'address',
+    'qualification',
+    'designation'
+];
+
+export const doctorFilterableFields: string[] = [
+    'searchTerm',
+    'email',
+    'contactNumber',
+    'gender',
+    'apointmentFee',
+    'specialties'
+];
+
+export const doctorFilterOptions: string[] = ['limit', 'page', 'sortBy', 'sortOrder'];
--- src/app/modules/doctor/doctor.controller.ts
+++ src/app/modules/doctor/doctor.controller.ts
@@ -2,11 +2,15 @@ import { Request, Response } from "express";
 import { doctorService } from "./doctor.service";
 import sendResponse from "../../../shared/sentResponse";
 import status from "http-status";
+import pick from "../../../shared/pick";
+import { doctorFilterableFields, doctorFilterOptions } from "./doctor.constant";
 
 
 const getAllFromDb = async (req: Request, res: Response) => {
-    const result = await doctorService.getAllFromDB();
+    const filters = pick(req.query, doctorFilterableFields);
+    const options = pick(req.query, doctorFilterOptions);
 
+    const result = await doctorService.getAllFromDB(filters, options);
     sendResponse(res, {
         statusCode: status.OK,
         success: true,
