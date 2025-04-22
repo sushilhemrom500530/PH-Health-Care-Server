2e2326a1435e19797bb3aa7d2f034f1aab478e98
Sushil Hemrom <sushilhemrom98@gmail.com>
2025-04-22 09:07:39 +0600

    add payment module

--- src/app/modules/appointment/appointment.routes.ts
+++ src/app/modules/appointment/appointment.routes.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { appointmentController } from "./appointment.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
 router.post(
     '/create',
+    auth(UserRole.PATIENT),
     appointmentController.insertIntoDB
 )
 
--- src/app/modules/appointment/appointment.services.ts
+++ src/app/modules/appointment/appointment.services.ts
@@ -1,11 +1,67 @@
+import prisma from "../../../shared/prisma";
 import { TTokenUser } from "../../interfaces"
-
+import { v4 as uuidv4 } from 'uuid';
 
 const getAllFromDB = async (user: TTokenUser, payload: any) => {
-    console.log('appointment created successfully')
+
 }
 const insertIntoDB = async (user: TTokenUser, payload: any) => {
-    console.log('appointment created successfully')
+    console.log('appointment created successfully', { payload });
+    const patientData = await prisma.patient.findUniqueOrThrow({
+        where: {
+            email: user.email
+        }
+    })
+    // console.log({ patientData })
+    const doctorData = await prisma.doctor.findUniqueOrThrow({
+        where: {
+            id: payload.doctorId
+        }
+    })
+
+    const doctorScheduleData = await prisma.doctorSchedules.findFirst({
+        where: {
+            doctorId: doctorData.id,
+            scheduleId: payload.scheduleId,
+            isBooked: false
+        }
+    })
+
+    const videoCallingId = uuidv4();
+    // console.log("uuid", videoCallingId)
+
+    const createData = {
+        patientId: patientData.id,
+        doctorId: doctorData.id,
+        scheduleId: payload.scheduleId,
+        videoCallingId
+    };
+
+    const result = await prisma.$transaction(async (tx) => {
+        const appointmentData = await prisma.appointment.create({
+            data: createData,
+            include: {
+                patient: true,
+                doctor: true,
+                schedule: true
+            }
+        })
+
+        await tx.doctorSchedules.update({
+            where: {
+                doctorId_scheduleId: {
+                    doctorId: doctorData.id,
+                    scheduleId: payload.scheduleId
+                }
+            },
+            data: {
+                isBooked: true,
+                appointmentId: appointmentData.id
+            }
+        })
+        return appointmentData;
+    })
+    return result;
 }
 
 export const appointmentService = {
