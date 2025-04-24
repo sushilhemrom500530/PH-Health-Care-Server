ce72856044096861eae5cca052222be53b55b0a5
Sushil Hemrom <sushilhemrom98@gmail.com>
2025-04-24 19:13:15 +0600

    add create review

--- src/app/modules/review/review.service.ts
+++ src/app/modules/review/review.service.ts
@@ -4,6 +4,8 @@ import { v4 as uuidv4 } from 'uuid';
 import { TPaginationOptions } from "../../interfaces/pagination";
 import calculatePagination from "../../../helpers/paginationHelper";
 import { Prisma, UserRole,AppointmentStatus,PaymentStatus,Prescription } from "@prisma/client";
+import apiError from './../../error/apiError';
+import { status } from 'http-status';
 
 
 
@@ -13,13 +15,30 @@ const getAllFromDB = async () => {
 
 
 const insertIntoDB = async (user:TTokenUser,payload:any) => {
-
-  const patientData = await prisma.patient.findUniqueOrThrow({
+    const patientData = await prisma.patient.findUniqueOrThrow({
+        where:{
+            email:user.email
+        }
+      })
+  const appointmentData = await prisma.appointment.findUniqueOrThrow({
     where:{
-        email:user.email
+        id:payload.appointmentId
     }
   })
-  console.log({patientData});
+  console.log({patientData,appointmentData});
+    if (!(patientData.id === appointmentData.patientId)) {
+        throw new apiError(status, "This is not your appointment!")
+    }
+    const result = await prisma.review.create({
+        data:{
+            appointmentId:appointmentData.id,
+            doctorId:appointmentData.doctorId,
+            patientId:appointmentData.patientId,
+            rating:payload.rating,
+            comment:payload.comment
+        }
+    });
+    return result;
 }
 
 
